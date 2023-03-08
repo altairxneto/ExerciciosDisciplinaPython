fd160c1c7981630d7c4c6233b0f381ea0e7461e0
Altair Neto <altairxneto@gmail.com>
2023-03-08 15:15:03 -0300

    Implementei o exercicio 5 e 6

---
+++ Lista 01/Exercicio05.py
@@ -0,0 +1,4 @@
+numero = int(input("Digite o numero para visualizar a tabuada:"))
+
+for numeros in range (1, 11):
+    print(numero, " X ", numeros, " = ", numero*numeros)
---
+++ Lista 01/Exercicio06.py
@@ -0,0 +1,7 @@
+altura = float(input("Digite a altura da parede:"))
+
+largura = float(input("Digite a largura da parede:"))
+
+print("A área da parede é: ", altura*largura, " m²")
+
+print("Será gasto um total de: ", altura*largura/2, " litros de tinta")
