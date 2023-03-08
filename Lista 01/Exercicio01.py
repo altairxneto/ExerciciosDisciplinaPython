e4fbb3163ebc9c607ea4b570c76cab3ce7416d5d
Altair Neto <altairxneto@gmail.com>
2023-03-08 13:46:32 -0300

    Implementei o exercicio 1 e 2

---
+++ Lista 01/Exercicio01.py
@@ -0,0 +1,6 @@
+numero = float(input("Digite o numero para mostrar seu sucessor e antecessor:"))
+
+sucessor = numero + 1
+antecessor = numero - 1
+
+print(f"O sucessor de {numero} é {sucessor} e o antecessor é {antecessor}")
---
+++ Lista 01/Exercicio02.py
@@ -0,0 +1,5 @@
+import math
+
+numero = float(input("Digite um numero:"))
+
+print("O dobro do numero digitado é: ", numero*2, ", o triplo é: ", numero*3,", e a raiz quadrada é: ", math.sqrt(numero))
